5d5dc8867c69f2d5382b247894fd148929b46735
Ceri Storey <ceri@lshift.net>
2016-02-04 14:19:43 +0000

    Note purpose.

--- src/main.rs
+++ src/main.rs
@@ -14,33 +14,33 @@ use std::collections::VecDeque;
 use clap::{Arg, App};
 
 #[derive(Debug)]
-enum MiChatCommand {
+enum ChainReplCommand {
     Broadcast(String),
     NewConnection(TcpStream),
 }
 
-struct MiChat {
+struct ChainRepl {
     connections: Slab<EventHandler>,
 }
 
-impl MiChat {
-    fn new() -> MiChat {
-        MiChat {
+impl ChainRepl {
+    fn new() -> ChainRepl {
+        ChainRepl {
             connections: Slab::new(1024),
         }
     }
 
-    fn listen(&mut self, event_loop: &mut mio::EventLoop<MiChat>, listener: TcpListener) {
+    fn listen(&mut self, event_loop: &mut mio::EventLoop<ChainRepl>, listener: TcpListener) {
         let l = EventHandler::Listener(Listener::new(listener));
         let token = self.connections.insert(l).expect("insert listener");
         &self.connections[token].register(event_loop, token);
     }
 
 
-    fn process_action(&mut self, msg: MiChatCommand, event_loop: &mut mio::EventLoop<MiChat>) {
+    fn process_action(&mut self, msg: ChainReplCommand, event_loop: &mut mio::EventLoop<ChainRepl>) {
         trace!("{:p}; got {:?}", self, msg);
         match msg {
-            MiChatCommand::Broadcast(s) => {
+            ChainReplCommand::Broadcast(s) => {
                 for c in self.connections.iter_mut() {
                     if let &mut EventHandler::Conn(ref mut c) = c {
                         c.enqueue(&s);
@@ -48,7 +48,7 @@ impl MiChat {
                 }
             },
 
-            MiChatCommand::NewConnection(socket) => {
+            ChainReplCommand::NewConnection(socket) => {
                 let token = self.connections
                     .insert_with(|token| EventHandler::Conn(Connection::new(socket, token)))
                     .expect("token insert");
@@ -82,7 +82,7 @@ impl Connection {
         }
     }
 
-    fn register(&self, event_loop: &mut mio::EventLoop<MiChat>, token: mio::Token) {
+    fn register(&self, event_loop: &mut mio::EventLoop<ChainRepl>, token: mio::Token) {
         event_loop.register_opt(
                 &self.socket,
                 token,
@@ -92,7 +92,7 @@ impl Connection {
     }
 
     // Event updates arrive here
-    fn handle_event(&mut self, _event_loop: &mut mio::EventLoop<MiChat>, events: mio::EventSet) {
+    fn handle_event(&mut self, _event_loop: &mut mio::EventLoop<ChainRepl>, events: mio::EventSet) {
         self.sock_status.insert(events);
         info!("Connection::handle_event: {:?}; this time: {:?}; now: {:?}",
                 self.socket.peer_addr(), events, self.sock_status);
@@ -100,8 +100,8 @@ impl Connection {
 
     // actions are processed here on down.
 
-    fn process_rules(&mut self, event_loop: &mut mio::EventLoop<MiChat>,
-        to_parent: &mut VecDeque<MiChatCommand>) {
+    fn process_rules(&mut self, event_loop: &mut mio::EventLoop<ChainRepl>,
+        to_parent: &mut VecDeque<ChainReplCommand>) {
         if self.sock_status.is_readable() {
             self.read();
             self.sock_status.remove(mio::EventSet::readable());
@@ -119,14 +119,14 @@ impl Connection {
         }
     }
 
-    fn process_buffer(&mut self, to_parent: &mut VecDeque<MiChatCommand>) {
+    fn process_buffer(&mut self, to_parent: &mut VecDeque<ChainReplCommand>) {
         let mut prev = 0;
         info!("{:?}: Read buffer: {:?}", self.socket.peer_addr(), self.read_buf);
         for n in self.read_buf.iter().enumerate()
                 .filter_map(|(i, e)| if *e == '\n' as u8 { Some(i) } else { None } ) {
             info!("{:?}: Pos: {:?}; chunk: {:?}", self.socket.peer_addr(), n, &self.read_buf[prev..n]);
             let s = String::from_utf8_lossy(&self.read_buf[prev..n]).to_string();
-            let cmd = MiChatCommand::Broadcast(s);
+            let cmd = ChainReplCommand::Broadcast(s);
             info!("Send! {:?}", cmd);
             to_parent.push_back(cmd);
             prev = n+1;
@@ -175,7 +175,7 @@ impl Connection {
         }
     }
 
-    fn reregister(&mut self, event_loop: &mut mio::EventLoop<MiChat>) {
+    fn reregister(&mut self, event_loop: &mut mio::EventLoop<ChainRepl>) {
         let mut flags = mio::EventSet::readable();
         if !self.write_buf.is_empty() {
             flags.insert(mio::EventSet::writable());
@@ -215,24 +215,24 @@ impl Listener {
         }
     }
 
-    fn register(&self, event_loop: &mut mio::EventLoop<MiChat>, token: mio::Token) {
+    fn register(&self, event_loop: &mut mio::EventLoop<ChainRepl>, token: mio::Token) {
         event_loop.register(&self.listener, token).expect("Register listener");
     }
 
-    fn handle_event(&mut self, _event_loop: &mut mio::EventLoop<MiChat>, events: mio::EventSet) {
+    fn handle_event(&mut self, _event_loop: &mut mio::EventLoop<ChainRepl>, events: mio::EventSet) {
         assert!(events.is_readable());
         self.sock_status.insert(events);
         info!("Listener::handle_event: {:?}; this time: {:?}; now: {:?}",
                 self.listener.local_addr(), events, self.sock_status);
     }
 
-    fn process_rules(&mut self, event_loop: &mut mio::EventLoop<MiChat>,
-            to_parent: &mut VecDeque<MiChatCommand>) {
+    fn process_rules(&mut self, event_loop: &mut mio::EventLoop<ChainRepl>,
+            to_parent: &mut VecDeque<ChainReplCommand>) {
         if self.sock_status.is_readable() {
             info!("the listener socket is ready to accept a connection");
             match self.listener.accept() {
                 Ok(Some(socket)) => {
-                    let cmd = MiChatCommand::NewConnection(socket);
+                    let cmd = ChainReplCommand::NewConnection(socket);
                     to_parent.push_back(cmd);
                 }
                 Ok(None) => {
@@ -259,14 +259,14 @@ enum EventHandler {
 }
 
 impl EventHandler {
-    fn handle_event(&mut self, _event_loop: &mut mio::EventLoop<MiChat>, events: mio::EventSet) {
+    fn handle_event(&mut self, _event_loop: &mut mio::EventLoop<ChainRepl>, events: mio::EventSet) {
         match self {
             &mut EventHandler::Conn(ref mut conn) => conn.handle_event(_event_loop, events),
             &mut EventHandler::Listener(ref mut listener) => listener.handle_event(_event_loop, events)
         }
     }
 
-    fn register(&self, event_loop: &mut mio::EventLoop<MiChat>, token: mio::Token) {
+    fn register(&self, event_loop: &mut mio::EventLoop<ChainRepl>, token: mio::Token) {
         match self {
             &EventHandler::Conn(ref conn) => conn.register(event_loop, token),
             &EventHandler::Listener(ref listener) => listener.register(event_loop, token)
@@ -274,8 +274,8 @@ impl EventHandler {
     }
 
 
-    fn process_rules(&mut self, event_loop: &mut mio::EventLoop<MiChat>,
-        to_parent: &mut VecDeque<MiChatCommand>) {
+    fn process_rules(&mut self, event_loop: &mut mio::EventLoop<ChainRepl>,
+        to_parent: &mut VecDeque<ChainReplCommand>) {
         match self {
             &mut EventHandler::Conn(ref mut conn) => conn.process_rules(event_loop, to_parent),
             &mut EventHandler::Listener(ref mut listener) => listener.process_rules(event_loop, to_parent)
@@ -290,7 +290,7 @@ impl EventHandler {
     }
 }
 
-impl mio::Handler for MiChat {
+impl mio::Handler for ChainRepl {
     type Timeout = ();
     type Message = ();
     fn ready(&mut self, event_loop: &mut mio::EventLoop<Self>, token: mio::Token, events: mio::EventSet) {
@@ -322,7 +322,7 @@ fn main() {
     if let Err(e) = log4rs::init_file(LOG_FILE, Default::default()) {
         panic!("Could not init logger from file {}: {}", LOG_FILE, e);
     }
-    let matches = App::new("michat")
+    let matches = App::new("chain-repl-test")
         .arg(Arg::with_name("bind").short("l").takes_value(true).required(true))
         .get_matches();
 
@@ -330,9 +330,9 @@ fn main() {
     let listener = TcpListener::bind(&address).expect("bind");
 
     let mut event_loop = mio::EventLoop::new().expect("Create event loop");
-    let mut service = MiChat::new();
+    let mut service = ChainRepl::new();
 
     service.listen(&mut event_loop, listener);
-    info!("running michat listener at: {:?}", address);
+    info!("running chain-repl-test listener at: {:?}", address);
     event_loop.run(&mut service).expect("Run loop");
 }
