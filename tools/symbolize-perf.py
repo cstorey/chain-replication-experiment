8920ccc64b26de76bb1ce03b545f22a301d10b66
Ceri Storey <ceri@lshift.net>
2016-05-16 16:46:44 +0100

    Cache symbol lookups.

--- tools/symbolize-perf.py
+++ tools/symbolize-perf.py
@@ -2,31 +2,43 @@
 
 import subprocess
 import re
+import sys
 
 STACK_LINE = re.compile('^\t *([a-f0-9]*) (\S+) \((.*)\)$')
 OUT_FMT = "\t %x %s (%s)"
 
-SYMBOLIZE = ['llvm-symbolizer-3.6', '-demangle=true']
+SYMBOLIZE = ['llvm-symbolizer-3.5', '-demangle=true']
 
 class Symbolicator(object):
 
   def __init__(self):
     self._child = subprocess.Popen(SYMBOLIZE, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    self._cache = {}
+    self.hits = 0
+    self.misses = 0
 
   def symbolize(self, obj, addr):
-    self._child.stdin.write("%s %#x\n" % (obj, addr))
-    while self._child.poll() is None:
-      line = self._child.stdout.readline()
-      if len(line.rstrip()) == 0:
-	break
-      symb = line.strip()
+    key = (obj, addr)
+    if key not in self._cache:
+        self.misses += 1
+        self._child.stdin.write("%s %#x\n" % (obj, addr))
+        lines = []
+        while self._child.poll() is None:
+          line = self._child.stdout.readline()
+          if len(line.rstrip()) == 0:
+            break
+          symb = line.strip()
 
-      line = self._child.stdout.readline()
-      if len(line.rstrip()) == 0:
-	break
-      posn = line.strip()
-      yield (symb, posn)
-
+          line = self._child.stdout.readline()
+          if len(line.rstrip()) == 0:
+            break
+          posn = line.strip()
+          lines.append((symb, posn))
+        self._cache[key] = lines
+        return lines
+    else:
+        self.hits += 1
+        return self._cache[key]
 
 def main(instr, out):
   symbolicator = Symbolicator()
@@ -45,6 +57,9 @@ def main(instr, out):
 	out.write('\n')
     else:
       out.write(line)
+
+  total = symbolicator.hits + symbolicator.misses
+  print >> sys.stderr, "hits: %2.2f%% of %d" % ((100 * symbolicator.hits / float(total)), total)
       
 if __name__ == '__main__':
   import sys
