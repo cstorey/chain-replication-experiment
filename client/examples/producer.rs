2900055b000d398c2c1188888c33d414a71a5c60
Ceri Storey <ceri@lshift.net>
2016-05-23 13:22:28 +0100

    Have the client take input from stdin/file.

--- client/examples/producer.rs
+++ client/examples/producer.rs
@@ -2,7 +2,10 @@
 extern crate clap;
 extern crate crexp_client;
 extern crate time;
+extern crate env_logger;
 use std::net::SocketAddr;
+use std::io::{self, Read, BufReader, BufRead};
+use std::fs::File;
 use time::{Duration, PreciseTime};
 
 use clap::{Arg, App, SubCommand};
@@ -12,22 +15,29 @@ use crexp_client::Producer;
 struct Void;
 
 fn main() {
+    env_logger::init().expect("env-logger init");
+
     let matches = App::new("producer")
         .arg(Arg::with_name("head").index(1))
-        .arg(Arg::with_name("messages").index(2).multiple(true))
+        .arg(Arg::with_name("file").short("f").takes_value(true))
         .get_matches();
 
     let head = value_t_or_exit!(matches.value_of("head"), SocketAddr);
-    let messages = values_t_or_exit!(matches.values_of("messages"), String);
+    let source = matches.value_of("file");
 
-    println!("Top level start");
+    println!("Top level start from {}", source.as_ref().unwrap_or(&"stdin"));
+    let src = source.map(|f| Box::new(File::open(f).expect("open source file")) as Box<Read>)
+            .unwrap_or_else(|| Box::new(io::stdin()) as Box<Read>);
+    let src = BufReader::new(src);
+
     let mut producer = Producer::new(head).expect("connect");
     println!("Got producer");
 
-    for msg in messages {
+    for msg in src.lines() {
+        let msg = msg.expect("input message");
         let start = PreciseTime::now();
-        println!("Producing: {:?}", msg);
-        let res = producer.publish(&msg).expect("produce");
+        println!("Producing: {:?}", msg.trim());
+        let res = producer.publish(msg.trim()).expect("produce");
         println!("Produce: {:?} -> {:?} in {}", msg, res, start.to(PreciseTime::now()));
     }
 }
