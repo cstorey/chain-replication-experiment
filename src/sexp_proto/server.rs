c52284f50d533f0bf040a4fbf3dd32b02559b6ff
Ceri Storey <ceri@lshift.net>
2016-10-11 11:45:34 +0100

    Further genericise errors in sexp server.

--- src/sexp_proto/server.rs
+++ src/sexp_proto/server.rs
@@ -5,10 +5,11 @@ use tokio::reactor::Handle;
 use futures::{Async, Poll, Future};
 use futures::stream;
 use std::io;
+use std::error;
 use std::net::SocketAddr;
+use spki_sexp as sexp;
 
 use super::sexp_proto::sexp_proto_new;
-use super::errors::Error;
 use void::Void;
 use serde;
 
@@ -19,12 +20,12 @@ struct SexpService<T> {
 struct SexpFuture<F>(F);
 
 impl<T> Service for SexpService<T>
-    where T: Service<Error = Error>,
+    where T: Service,
           T::Future: Send + 'static
 {
     type Request = T::Request;
-    type Response = proto::Message<T::Response, stream::Empty<Void, Error>>;
-    type Error = Error;
+    type Response = proto::Message<T::Response, stream::Empty<Void, T::Error>>;
+    type Error = T::Error;
     // type Future = Box<Future<Item = Self::Response, Error = Error> + Send + 'static>;
     type Future = SexpFuture<T::Future>;
 
@@ -37,7 +38,7 @@ impl<T> Service for SexpService<T>
 }
 
 impl<F: Future> Future for SexpFuture<F> {
-    type Item = proto::Message<F::Item, stream::Empty<Void, Error>>;
+    type Item = proto::Message<F::Item, stream::Empty<Void, F::Error>>;
     type Error = F::Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
@@ -47,8 +48,9 @@ impl<F: Future> Future for SexpFuture<F> {
 }
 
 pub fn serve<T>(handle: &Handle, addr: SocketAddr, new_service: T) -> io::Result<ServerHandle>
-    where T: NewService<Error = Error> + Send + 'static,
+    where T: NewService + Send + 'static,
           <<T as NewService>::Item as Service>::Future: Send,
+          <T as NewService>::Error: Send + error::Error + From<proto::Error<T::Error>> + From<sexp::Error>,
           T::Request: serde::Deserialize + serde::Serialize,
           T::Response: serde::Deserialize + serde::Serialize
 {
