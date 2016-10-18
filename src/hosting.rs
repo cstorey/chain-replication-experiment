869f2992cb4a79c0d05293cb19a2702dd3a96421
Ceri Storey <ceri@lshift.net>
2016-10-18 15:05:18 +0100

    Start sketching out the Spherical Bovine network.

--- src/hosting.rs
+++ src/hosting.rs
@@ -29,6 +29,11 @@ pub struct CoreService {
 
 pub struct SexpHost;
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct BovineAddr(pub usize);
+
+pub struct SphericalBovine;
+
 impl CoreService {
     pub fn new() -> Self {
         let store = RamStore::new();
@@ -56,6 +61,31 @@ impl Host for SexpHost {
         Ok(HostConfig {
             head: head_host.local_addr().clone(),
             tail: tail_host.local_addr().clone(),
+        })
+    }
+}
+
+impl SphericalBovine {
+    pub fn new() -> Self {
+        SphericalBovine
+    }
+}
+impl Host for SphericalBovine {
+    type Addr = BovineAddr;
+
+    fn build_server(&mut self,
+                    service: CoreService,
+                    handle: &Handle,
+                    head_addr: Self::Addr,
+                    tail_addr: Self::Addr)
+                    -> Result<HostConfig<Self::Addr>, io::Error> {
+        let CoreService { head, tail } = service;
+        let head_host = unimplemented!();
+        let tail_host = unimplemented!();
+
+        Ok(HostConfig {
+            head: head_addr,
+            tail: tail_addr,
         })
     }
 }
--- tests/smoketest.rs
+++ tests/smoketest.rs
@@ -16,7 +16,7 @@ use vastatrix::LogPos;
 use vastatrix::hosting::*;
 
 #[test]
-fn smoketest() {
+fn smoketest_tcp() {
     env_logger::init().unwrap_or(());
 
     let mut core = Core::new().unwrap();
@@ -35,6 +35,45 @@ fn smoketest() {
     info!("Head at: {:?}, tail at: {:?}",
           server.head,
           server.tail);
+    let client = vastatrix::ThickClient::new(core.handle(),
+                                             &server.head,
+                                             &server.tail);
+
+    let f = client.log_item(b"hello".to_vec())
+                  .and_then(|pos0| {
+                      client.log_item(b"world".to_vec()).map(move |pos1| (pos0, pos1))
+                  });
+    let (wpos0, wpos1) = core.run(f).expect("run write");
+
+    info!("Wrote to offset:{:?}", (wpos0, wpos1));
+
+    let item_f = client.fetch_next(LogPos::zero())
+                       .and_then(|(pos0, val0)| {
+                           client.fetch_next(pos0).map(move |second| vec![(pos0, val0), second])
+                       });
+
+    let read = core.run(item_f).expect("run read");
+    info!("Got: {:?}", read);
+
+    assert_eq!(read,
+               vec![(wpos0, b"hello".to_vec()), (wpos1, b"world".to_vec())]);
+}
+
+#[cfg(never)]
+#[test]
+fn smoketest_bovine() {
+    env_logger::init().unwrap_or(());
+
+    let mut core = Core::new().unwrap();
+
+    let service = CoreService::new();
+
+    let net = SphericalBovine::new();
+
+    let server = net.build_server(service, &core.handle(), 0, 1).expect("start server");
+    println!("running: {:?}", server);
+
+    info!("Head at: {:?}, tail at: {:?}", server.head, server.tail);
     let client = vastatrix::ThickClient::new(core.handle(),
                                              &server.head,
                                              &server.tail);
