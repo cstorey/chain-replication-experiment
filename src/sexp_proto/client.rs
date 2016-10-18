d6360b9a6a564b072eef5004c0bc5574dad36938
Ceri Storey <ceri@lshift.net>
2016-10-18 16:00:36 +0100

    Factor out client tcp-related bits.

--- src/sexp_proto/client.rs
+++ src/sexp_proto/client.rs
@@ -1,8 +1,10 @@
-use futures::{Async, Future};
+use futures::{Async, IntoFuture, Future};
 use std::net::SocketAddr;
 use service::Service;
 use proto::{self, pipeline};
+use proto::pipeline::Frame;
 use tokio::reactor::Handle;
+use tokio::io::FramedIo;
 use tokio::net::TcpStream;
 use futures::stream;
 use serde;
@@ -12,40 +14,51 @@ use super::errors::Error;
 use void::Void;
 
 use std::fmt;
+use std::io;
 
-pub struct Client<T, R> {
-    inner: proto::Client<T, R, stream::Empty<Void, Error>, Error>,
+pub struct Client<Req, Res> {
+    inner: proto::Client<Req, Res, stream::Empty<Void, Error>, Error>,
 }
 
-impl<T: Send + 'static, R: Send + 'static> Service for Client<T, R> {
-    type Request = T;
-    type Response = R;
+impl<Req: Send + 'static, Res: Send + 'static> Service for Client<Req, Res> {
+    type Request = Req;
+    type Response = Res;
     type Error = Error;
     type Future = Box<Future<Item = Self::Response, Error = Error> + Send>;
 
     fn poll_ready(&self) -> Async<()> {
-        Async::Ready(())
+        self.inner.poll_ready()
     }
-    fn call(&self, req: T) -> Self::Future {
+    fn call(&self, req: Req) -> Self::Future {
         self.inner.call(proto::Message::WithoutBody(req))
     }
 }
 
-impl<T, R> fmt::Debug for Client<T, R> {
+impl<Req, Res> fmt::Debug for Client<Req, Res> {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         fmt.debug_tuple("Client").finish()
     }
 }
 
-pub fn connect<T, R>(handle: Handle, addr: &SocketAddr) -> Client<T, R>
-    where T: Send + 'static + serde::ser::Serialize,
-          R: Send + 'static + serde::de::Deserialize
+pub fn connect<Req, Res>(handle: Handle, addr: &SocketAddr) -> Client<Req, Res>
+    where Req: Send + 'static + serde::ser::Serialize,
+          Res: Send + 'static + serde::de::Deserialize
 {
     let addr = addr.clone();
     let h = handle.clone();
 
     let builder = move || TcpStream::connect(&addr, &h).map(|s| sexp_proto_new(s));
+    connect_with(builder, handle)
+}
 
+pub fn connect_with<B, F, T, Req, Res>(builder: B, handle: Handle) -> Client<Req, Res>
+    where B: Fn() -> F,
+          F: IntoFuture<Item = T, Error = io::Error>,
+          F::Future: Send + 'static,
+          T: FramedIo<In = Frame<Req, Void, Error>, Out = Frame<Res, Void, Error>> + Send + 'static,
+          Req: Send + 'static,
+          Res: Send + 'static
+{
     let client = pipeline::connect(builder, &handle);
     Client { inner: client }
 }
