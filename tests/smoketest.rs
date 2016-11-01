fd24cafca523eeab62f08a18dca8bd2a4ed5268d
Ceri Storey <ceri@lshift.net>
2016-11-01 15:31:40 +0000

    Three member chains now work.

--- tests/smoketest.rs
+++ tests/smoketest.rs
@@ -114,7 +114,6 @@ fn smoketest_two_member_chain() {
 }
 
 #[test]
-#[ignore]
 fn smoketest_three_member_chain() {
     env_logger::init().unwrap_or(());
     let timer = tokio_timer::wheel().tick_duration(Duration::from_millis(1)).build();
@@ -125,8 +124,8 @@ fn smoketest_three_member_chain() {
     let mut head_addr = local_anon_addr.clone();
     let mut tail_addr = local_anon_addr.clone();
 
-    head_addr.set_port(11000);
-    tail_addr.set_port(11001);
+    head_addr.set_port(11300);
+    tail_addr.set_port(11301);
     let server0 = build_server(
                       &core.handle(),
                       head_addr.clone(),
@@ -134,8 +133,8 @@ fn smoketest_three_member_chain() {
         .expect("start server");
     println!("running: {:?}", server0);
 
-    head_addr.set_port(11010);
-    tail_addr.set_port(11011);
+    head_addr.set_port(11310);
+    tail_addr.set_port(11311);
     let server1 = build_server(
                       &core.handle(),
                       head_addr.clone(),
@@ -144,8 +143,8 @@ fn smoketest_three_member_chain() {
 
     println!("running: {:?}", server1);
 
-    head_addr.set_port(11020);
-    tail_addr.set_port(11021);
+    head_addr.set_port(11320);
+    tail_addr.set_port(11321);
     let server2 = build_server(
                       &core.handle(),
                       head_addr.clone(),
@@ -158,21 +157,17 @@ fn smoketest_three_member_chain() {
     let f = client.add_peer(server0.clone())
         .and_then(|_| client.add_peer(server1.clone()))
         .and_then(|_| client.log_item(b"hello".to_vec()))
-        .and_then(|_| client.add_peer(server2.clone()))
+        .and_then(|pos0| client.add_peer(server2.clone()).map(move |_| pos0))
         .and_then(|pos0| client.log_item(b"world".to_vec()).map(move |pos1| (pos0, pos1)));
     let (wpos0, wpos1) = core.run(timer.timeout(f, timeout)).expect("run write");
 
     info!("Wrote to offset:{:?}", (wpos0, wpos1));
 
-    let item_f = client.fetch_next(LogPos::zero())
-        .and_then(|(pos0, val0)| {
-            client.fetch_next(pos0).map(move |second| vec![(pos0, val0), second])
-        });
+    let (pos0, val0) = core.run(timer.timeout(client.fetch_next(LogPos::zero()), timeout)).expect("fetch first");
+    info!("Got: {:?}", (&pos0, &val0));
+    let (pos1, val1) = core.run(timer.timeout(client.fetch_next(pos0), timeout)).expect("fetch second");
+    info!("Got: {:?}", (&pos1, &val1));
 
-    let res = core.run(timer.timeout(item_f, timeout));
-    let read = res.expect("run read");
-    info!("Got: {:?}", read);
-
-    assert_eq!(read,
-               vec![(wpos0, b"hello".to_vec()), (wpos1, b"world".to_vec())]);
+    assert_eq!(vec![(pos0, &*String::from_utf8_lossy(&val0)), (wpos1, &*String::from_utf8_lossy(&val1))],
+               vec![(wpos0, "hello"), (wpos1, "world")]);
 }
